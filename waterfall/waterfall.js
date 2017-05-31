b42dddbc49f78bbb6a2b39e48bd62cb590228cd2
sogokyo <4440086e@opayq.com>
2017-05-31 20:26:19 +0900

    Add files via upload

---
+++ waterfall/waterfall.js
@@ -0,0 +1,324 @@
+function waterfallChart(data,stylename,media,yMin,yMax,plotpadding,legAlign,yHighlight, labels, numTicksy, yAlign){
+
+    var titleYoffset = d3.select("#"+media+"Title").node().getBBox().height
+    var subtitleYoffset=d3.select("#"+media+"Subtitle").node().getBBox().height;
+
+    var groupNames=[]
+    for(i = 0; i< data.length; i++){    
+        if(groupNames.indexOf(data[i].group) === -1){
+            groupNames.push(data[i].group);        
+        }        
+    }
+
+    d3.selection.prototype.moveToFront = function() {
+        return this.each(function(){
+        this.parentNode.appendChild(this);
+        });
+    }
+
+    //Select the plot space in the frame from which to take measurements
+    var chart=d3.select("#"+media+"chart")
+    var plot=d3.select("#"+media+"plot")
+
+    var yOffset=d3.select("#"+media+"Subtitle").style("font-size");
+    yOffset=Number(yOffset.replace(/[^\d.-]/g, ''));
+    
+    //Get the width,height and the marginins unique to this plot
+    var w=plot.node().getBBox().width;
+    var h=plot.node().getBBox().height;
+    var margin=plotpadding.filter(function(d){
+        return (d.name === media);
+      });
+    margin=margin[0].margin[0];
+
+    var colours= d3.scale.ordinal()
+      .domain(groupNames)
+      .range(stylename.fillcolours);
+
+    var plotWidth=w-margin.left-margin.right
+    var plotHeight=h-margin.top-margin.bottom
+    //Work out xdomain
+
+    console.log(yMin,yMax)
+    var cumulative =0;
+
+    function extents(last,value) {
+
+        if (last==0){
+            return [0,value]
+        }
+        else {
+            if (value>0){
+
+                return [last, cumulative+value]
+            } 
+            else {
+
+                return [last+(value), last +(value)+Math.abs(value)]}    
+        }
+    }
+
+
+    function group(value) {
+        return value < 0 ? 'negative' : 'positive';
+    }
+
+    var plotData=data.map(function(d) {
+        yMin=Math.min(cumulative,yMin);
+        yMax=Math.max(cumulative,yMax);
+
+
+        var extent = extents(cumulative,+d.value);
+        cumulative=extent[1];
+
+        if(d.value<0){
+            cumulative=extent[0];
+        }
+        else {
+            cumulative=extent[1]};
+
+        return {
+            cat:d.cat,
+            value: +d.value,
+            start: extent[0],
+            end: extent[1],
+            group: group(d.value)
+        }
+    })
+
+    plotData.push({
+        cat: 'Total',
+        value:cumulative,
+        start: 0,
+        end: d3.sum(data, function(d){
+            return d3.format(".2f")(d.value);
+        }),
+        group: null
+    })
+
+    var yScale = d3.scale.linear()
+        .range([plotHeight, 0]);
+    //var max=d3.max(data, function(d,i) { return +d.value;});
+    yScale.domain([yMin, yMax]);
+
+    var yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient(yAlign)
+    .ticks(numTicksy);
+
+    var yLabel=plot.append("g")
+      .attr("class", media+"yAxis")
+      .call(yAxis)
+
+    //calculate what the ticksize should be now that the text for the labels has been drawn
+    var yLabelOffset=yLabel.node().getBBox().width
+    var yticksize=colculateTicksize(yAlign, yLabelOffset);
+
+    yLabel.call(yAxis.tickSize(yticksize))
+    yLabel
+        .attr("transform",function(){
+            if (yAlign=="right"){
+                return "translate("+(margin.left)+","+margin.top+")"
+            }
+            else return "translate("+(w-margin.right)+","+margin.top+")"
+            })
+
+    //identify 0 line if there is one
+    var originValue = 0;
+    var origin = plot.selectAll(".tick").filter(function(d, i) {
+            return d==originValue || d==yHighlight;
+        })
+    .classed(media+"origin",true);
+
+    var xScale = d3.scale.ordinal()
+    .rangeRoundBands([0, plotWidth],.3);
+
+    var xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient("bottom")
+    .tickSize(0);
+
+    xScale.domain(plotData.map(function(d) { return d.cat;}));
+
+
+    var xLabels=plot.append("g")
+      .attr("class", media+"xAxis")
+      .attr("transform", "translate("+(margin.left)+"," + (h-margin.bottom) + ")")
+      .attr("text-anchor","middle")
+      .call(xAxis)
+      .selectAll("text")
+      .attr("dy","1em");
+
+    plot.selectAll("."+media+"fill")
+    .data(plotData)
+    .enter()
+        .append("g")
+        .attr("id",function(d) { return d.cat+"-"+d.value; })
+        .attr("transform",function(){
+                return "translate("+(margin.left)+","+margin.top+")"
+            })
+        .call(function(parent){
+            parent.append('rect')
+                .style("fill", function (d) {
+                    return colours(d.group)
+                })
+                .attr("id",function(d) { return d.cat+"-"+d.value; })
+                .attr("class",media+"fill")
+                .attr("x", function(d) { return xScale(d.cat); })
+                .attr("width", xScale.rangeBand())
+                .attr("y", function(d) { return yScale(d.start)-(yScale(d.start)-yScale(d.end))})
+                .attr("height", function(d){return yScale(d.start)-yScale(d.end); })
+                .on("mouseover",pointer)
+                .on("click",function(d){
+                    var elClass = d3.select(this)
+                    if (elClass.attr("class")==media+"fill") {
+                        d3.select(this).attr("class",media+"highlight");
+                        d3.select(this).style("fill",colours.range()[1])
+                    }
+                    else{var el=d3.select(this)
+                        el.attr("class",media+"fill");
+                        d3.select(this).style("fill",colours.range()[0])
+                    }
+                })
+             
+             parent.filter(function(d,i) {return d.cat != "Total" && i<(plotData.length-2)})
+             .append("line")
+                .attr("class", media+"whiskers")
+                .attr("x1", function(d,i) {return xScale(d.cat)})
+                .attr("y1", function(d) { 
+                    if(d.value>0){
+                        return yScale(d.end)
+                    }
+                    else {return yScale(d.start)}
+                })
+                .attr("x2", function(d,i) {return xScale(d.cat)+(xScale.rangeBand()*2.4)})
+                .attr("y2", function(d) { 
+                    if(d.value>0){
+                        return yScale(d.end)
+                    }
+                    else {return yScale(d.start)}
+                })
+
+            var el=d3.selectAll("."+media+"whiskers")
+            el.moveToFront()
+
+            
+            // parent.append("path")
+            //     .attr("class",media+"barlinks")
+            //     .attr("d",function (d){
+            //         if(d.start==0){
+            //             return "M"+(xScale(d.cat))+","+(yScale(d.end))+"L"+(1/xScale.rangeBand())+","+(yScale(d.end))
+            //         }
+            //         else {return "M"+(xScale(d.cat))+","+(yScale(d.start))+"L"+(1*xScale.rangeBand())+","+(yScale(d.start))}
+            //     })
+
+            if (labels) {
+                parent.append("text")
+                .attr("class", media+"label")
+                .style("text-anchor","middle")
+                .text(function(d) {return d3.format(".1f")(d.value);})
+                .attr("x", function(d) { return xScale(d.cat)+(xScale.rangeBand()/2)})
+                .attr("y", function(d) {
+                    if(d.value>0) {
+                        return yScale(d.start)-(yScale(d.start)-yScale(d.end))+yOffset+yOffset/2
+                    }
+                    else {
+                        return yScale(d.start)-yOffset/2}
+                });
+                var clear = yLabel.selectAll(".tick").filter(function(d, i) {
+                    return d!=originValue
+                })
+                clear.remove()
+            }
+
+        });
+
+    //Add labels so that the preflight script in illustrator will work
+    d3.selectAll(".printxAxis text")
+    .attr("id","xAxisLabel")
+    d3.selectAll(".printyAxis text")
+    .attr("id","yAxisLabel")
+    d3.selectAll(".printyAxis line")
+    .attr("id","yAxisTick")
+    d3.selectAll(".printxAxis line")
+    .attr("id","xAxisTick")
+    d3.selectAll(".printminorAxis line")
+    .attr("id","minorTick")
+
+    d3.selectAll(".domain").remove()
+
+    //create a legend first
+    if (groupNames[0]!="-") {
+        var legendyOffset=0
+        var legend = plot.append("g")
+            .attr("id",media+"legend")
+            .on("mouseover",pointer)
+            // .selectAll("g")
+            .data(groupNames)
+            .enter()
+            .append("g")
+            .attr ("id",function(d,i){
+                return media+"l"+i
+            })
+
+        var drag = d3.behavior.drag().on("drag", moveLegend);
+        d3.select("#"+media+"legend").call(drag);
+            
+        legend.append("text")
+
+            .attr("id",function(d,i){
+                return media+"t"+i
+            })
+            .attr("x",yOffset+yOffset/5)
+            .attr("y",0)
+            .attr("class",media+"legend")
+            .text(function(d){
+                return d;
+            })
+
+        legend.append("rect")
+            .attr("x",0)
+            .attr("y",-yOffset+yOffset/3)
+            .attr("width",(yOffset/100)*85)
+            .attr("height",(yOffset/100)*70)
+            .style("fill", function(d,i){return colours(d)})
+
+        legend.attr("transform",function(d,i){
+            if (legAlign=='hori') {
+                var gHeigt=d3.select("#"+media+"l0").node().getBBox().height;
+                if (i>0) {
+                    var gWidth=d3.select("#"+media+"l"+(i-1)).node().getBBox().width+15; 
+                }
+                else {gWidth=0};
+                legendyOffset=legendyOffset+gWidth;
+                return "translate("+(legendyOffset)+","+(gHeigt)+")";  
+            }
+            else {
+                var gHeight=d3.select("#"+media+"l"+(i)).node().getBBox().height
+                return "translate(0,"+((i*yOffset)+yOffset/2)+")"};
+        })
+    }
+    
+
+    
+    function colculateTicksize(align, offset) {
+        if (align=="right") {
+            return w-margin.left-offset
+        }
+        else {return w-margin.right-offset}
+    }
+
+
+    function pointer() {
+        this.style.cursor='pointer'
+    }
+
+    function moveLegend() {
+        var dX = d3.event.x; // subtract cx
+        var dY = d3.event.y; // subtract cy
+        d3.select(this).attr("transform", "translate(" + dX + ", " + dY + ")");
+
+    }
+
+
+}
---
+++ xy-heatmap-quant/styles.js
@@ -0,0 +1,96 @@
+
+var styleprint = {classes:
+[{class:"background{fill: none}"},
+{class:"chartholder{fill: none}"},
+{class:"title{font-size:12px; fill: #000000; font-weight: 600;}"},
+{class:"subtitle{fill:#000000; font-size: 9.6px;}"},
+{class:"source{fill: #000000; font-size: 7.2px;}"},
+{class:"logo{fill: #dacfc5;}"},
+{class:"hat{fill:none; stroke-width:0.3px; stroke:#22190f;}"},
+{class:"xAxis{fill:none; stroke-width:0px; stroke:none;}"},
+{class:"xAxis line{fill:none; stroke-width:.3px; stroke:#000000;}"},
+{class:"xAxis text{fill:#000000; font-size: 9.6px; font-feature-settings:'tnum' 1;}"},
+{class:"yAxis{fill:none; stroke-width:0px; stroke:none;}"},
+{class:"yAxis line{fill:none; stroke-width:0.3px; stroke:#22190f;}"},
+{class:"yAxis text{fill:#000000; font-size: 9.6px; font-feature-settings: 'tnum' 1;}"},
+{class:"origin line{fill:none; stroke-width:0.6px;}"},
+{class:"lines{fill:none; stroke-width:2.0px;stroke-linecap: round;}"}
+],
+titleOffset:10,
+subOffset:0,
+legendyOffset:0,
+fillcolours:["#004d6d","#64a2c1","#d9e7ef","#EFF8F5","#000000"]
+}
+
+var styleweb = {classes:
+[{class:"background{fill: none}"},
+{class:"chartholder{fill: none;}"},
+{class:"title{font-size:25.0px; fill: #3b3d3b;}"},
+{class:"subtitle{font-size:18.0px; fill: #6b6e68;}"},
+{class:"source{fill: #6b6e68; font-size: 14.0px;}"},
+{class:"logo{fill: #dacfc5;}"},
+{class:"xAxis{fill:none; stroke: none;}"},
+{class:"xAxis line{fill:none; stroke-width: 1.0px; stroke:#c3bcb0;}"},
+{class:"xAxis text{fill:#6b6e68; font-size: 18.0px;}"},
+{class:"yAxis{fill:none; stroke: none;}"},
+{class:"yAxis line{fill:none; stroke-width: 1.0px; stroke:#c3bcb0; stroke-dasharray:1,2}"},
+{class:"yAxis text{fill:#6b6e68; font-size: 18.0px; font-feature-settings: 'tnum' 1; text-anchor: end;}"},
+{class:"origin line{fill:none; stroke:#9ba497; stroke-width: 1px;stroke-dasharray:1,0}"},
+{class:"lines{fill:none; stroke-width: 2.5px; stroke-linecap: round;}"}
+],
+titleOffset:21,
+subOffset:0,
+legendyOffset:0,
+fillcolours:["#993355","#c28693","#fae9dd","#C1B8B9","#e2c3c1"]
+}
+
+var stylesoc = {classes:
+[{class:"background{fill: #212121;}"},
+{class:"chartholder{fill: none;}"},
+{class:"title{font-size: 38px; fill: #ffffff; opacity: 0.9; font-weight: 500;}"},
+{class:"subtitle{font-size: 28px; fill: #ffffff; opacity: 0.70; font-weight: 100; line-height: 30px;}"},
+{class:"source{font-size: 25px; fill: #ffffff; opacity: 0.50; font-weight: 100; line-height: 30px;}"},
+{class:"logo{fill: #ffffff; opacity: 0.38;}"},
+{class:"xAxis{fill:none; stroke: none;}"},
+{class:"xAxis line{fill:none; stroke:#ffffff; stroke-width: 2.0px; opacity: 0.5;}"},
+{class:"xAxis text{font-size: 28px; fill: #ffffff; opacity: 0.50; font-weight: 100; line-height: 18px;}"},
+{class:"yAxis{fill:none; stroke: none;}"},
+{class:"yAxis line{fill:none; stroke-width: 2.0px; stroke-dasharray:2,8; opacity: 0.38; stroke:#ffffff;}"},
+{class:"yAxis text{font-size: 28px; fill: #ffffff; opacity: 0.50; font-weight: 100; line-height: 28px; font-feature-settings: 'tnum' 1; text-anchor: end;}"},
+{class:"origin line{fill:none; stroke:#ffffff; stroke-width: 2.0px; opacity: 0.5; stroke-dasharray:1,0}"},
+{class:"lines{fill:none; stroke-width: 5px;  stroke-linecap: round;}"}
+],
+titleOffset:36,
+subOffset:0,
+legendyOffset:0,
+fillcolours:["#CB4759","#D57175","#E09697","#EDBFC0","#FDEDF4"]
+}
+var stylevid = {classes:
+[{class:"background{fill: #212121;}"},
+{class:"titleframe{fill: none;}"},
+{class:"chartholder{fill: none;}"},
+{class:"title{font-size: 68px; fill: #ffffff; font-weight: 600;opacity: 0.9;}"},
+{class:"subtitle{font-size: 48; fill: #ffffff; font-weight: 400;opacity: 0.7;}"},
+{class:"source{font-size: 36; fill: #ffffff; font-weight: 400;opacity: 0.5;}"},
+{class:"labels{font-size: 48px; fill: #ffffff; opacity: 0.5; font-weight: 400;}"},
+{class:"logo{fill: #ffffff;}"},
+{class:"xAxis{fill:none; stroke: none;}"},
+{class:"xAxis line{fill:none; stroke-width: 4px; stroke:#ffffff;opacity: 0.5}"},
+{class:"xAxis text{font-size: 48; fill: #ffffff; font-weight: 400;opacity: 0.5}"},
+{class:"yAxis{fill:none; stroke: none;}"},
+{class:"yAxis line{fill:none; stroke-width: 4px; stroke:#ffffff;opacity: 0.38}"},
+{class:"yAxis text{font-size: 48; fill: #ffffff; font-weight: 400;font-feature-settings: 'tnum' 1; text-anchor: end;opacity: 0.5}"},
+{class:"origin line{fill:none; stroke:#ffffff; stroke-width: 5.0px;stroke-dasharray:1,0; opacity: 0.5}"},
+{class:"whiskers{fill:none; stroke-width:2.0px;stroke:#ffffff; opacity: 0.5}"},
+{class:"lines{fill:none; stroke-width: 8.0px; stroke-linecap: round;}"},
+{class:"fill{fill-opacity:1.0; stroke-width: 0.0px}"},
+{class:"highlight{fill-opacity:1.0;}"},
+{class:"day{fill:none; stroke: #ffffff; stroke-width:4.0px; opacity: 0.5; stroke-linecap: round;}"},
+{class:"month{fill:none; stroke: #ffffff; stroke-width:4.0px; opacity: 0.5; stroke-linecap: round;}"}
+
+],
+titleOffset:68,
+subOffset:0,
+legendyOffset:15,
+fillcolours:["#CB4759","#D57175","#E09697","#EDBFC0","#FDEDF4"]
+}
