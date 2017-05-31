8914469423ecd482f0edd4489a94a3857c1ed40b
sogokyo <4440086e@opayq.com>
2017-05-31 20:24:00 +0900

    Add files via upload

---
+++ sankey/styles.js
@@ -0,0 +1,114 @@
+var styleprint = {classes:
+[{class:"background{fill: none}"},
+{class:"chartholder{fill: none}"},
+{class:"title{font-size:12px; fill: #000000; font-weight: 600;}"},
+{class:"subtitle{font-family:Avenir; font-style:italic; fill:#000000; font-size:7.5px; line-height:9px; font-weight:100; letter-spacing:-.2px;}"},
+{class:"legend{font-family:Avenir; font-size:8px; line-height:9px; font-weight: 100; fill: #000; letter-spacing:-.2px;}"},
+{class:"label{fill:#000000; font-size: 9.6px;font-weight: 500;}"},
+{class:"source{fill: #000000; font-size: 7.2px;}"},
+{class:"logo{fill: #dacfc5;}"},
+{class:"hat{fill:none; stroke-width:0.3px; stroke:#22190f;}"},
+{class:"xAxis{fill:none; stroke-width:0px; stroke:none;}"},
+{class:"xAxis line{fill:none; stroke-width:.3px; stroke:#000000;}"},
+{class:"xAxis text{fill:#000000; font-size: 9.6px;}"},
+{class:"yAxis{fill:none; stroke-width:0px; stroke:none;}"},
+{class:"yAxis line{fill:none; stroke-width:0.3px; stroke:#22190f;}"},
+{class:"yAxis text{fill:#000000; font-size: 9.6px; font-feature-settings: 'tnum' 1; text-anchor: end;}"},
+{class:"origin line{fill:none; stroke-width:0.6px;}"},
+{class:"link{fill:none; opacity:0.5;}"},
+{class:"circles{opacity: .38}"},
+{class:"circlesHighlight{opacity: 1.0}"}
+],
+titleOffset:10,
+subOffset:0,
+legendyOffset:0,
+linecolours:["#154577","#58bdbb","#cabd92","#ac252a","#ee5427","#f9a71a","#b0d480","#009a8e","#00809c","#675388","#a16698","#d2d3d3"],
+}
+
+var styleweb = {classes:
+[{class:"background{fill: none;}"},
+{class:"chartholder{fill: none;}"},
+{class:"title{font-size:25.0px; fill: #3b3d3b; font-weight: 100; letter-spacing:-.2px;}"},
+{class:"subtitle{font-size:18.0px; fill: #6b6e68; font-weight: 100; letter-spacing:-.2px;}"},
+{class:"legend{font-family:Avenir; font-size:12.75px; line-height:15.3px; font-weight: 100; fill: #000;}"},
+{class:"label{font-size:14px; fill: #6b6e68; font-weight: 400;}"},
+{class:"source{fill: #6b6e68; font-size: 14.0px; font-weight: 100;}"},
+{class:"logo{fill: #dacfc5;}"},
+{class:"xAxis{fill:none; stroke: none;}"},
+{class:"xAxis line{fill:none; stroke-width: 1.0px; stroke:#c3bcb0;}"},
+{class:"xAxis text{fill:#6b6e68; font-size: 18.0px;}"},
+{class:"yAxis{fill:none; stroke: none;}"},
+{class:"yAxis line{fill:none; stroke-width: 1.0px; stroke:#c3bcb0; stroke-dasharray:1,2}"},
+{class:"yAxis text{fill:#6b6e68; font-size: 18.0px; font-feature-settings: 'tnum' 1; text-anchor: end;}"},
+{class:"origin line{fill:none; stroke:#9ba497; stroke-width: 1px;stroke-dasharray:1,0}"},
+{class:"link{fill:none; opacity:0.5;}"},
+{class:"circles{opacity: .38}"},
+{class:"circlesHighlight{opacity: 0.8}"}
+],
+titleOffset:21,
+subOffset:0,
+legendyOffset:0,
+linecolours:["#154577","#58bdbb","#cabd92","#ac252a","#ee5427","#f9a71a","#b0d480","#009a8e","#00809c","#675388","#a16698","#d2d3d3"],
+}
+
+var stylesoc = {classes:
+[{class:"background{fill: #212121;}"},
+{class:"chartholder{fill: none;}"},
+{class:"title{font-size: 38px; fill: #ffffff; opacity: 0.95; font-weight: 400;}"},
+{class:"subtitle{font-size: 28px; fill: #ffffff; opacity: 0.70; font-weight: 100; line-height: 30px;}"},
+{class:"label{font-size:28px; fill:#ffffff; opacity: 0.70; font-weight: 400;}"},
+{class:"source{font-size: 25px; fill: #ffffff; opacity: 0.50; font-weight: 100;}"},
+{class:"logo{fill: #ffffff; opacity: 0.38;}"},
+{class:"xAxis{fill:none; stroke: none;}"},
+{class:"xAxis line{fill:none; stroke:#ffffff; stroke-width: 2.0px; opacity: 0.5;}"},
+{class:"xAxis text{font-size: 28px; fill: #ffffff; opacity: 0.50; font-weight: 100; line-height: 18px;}"},
+{class:"yAxis{fill:none; stroke: none;}"},
+{class:"yAxis line{fill:none; stroke-width: 2.0px; stroke-dasharray:2,8; opacity: 0.38; stroke:#ffffff;}"},
+{class:"yAxis text{font-size: 28px; fill: #ffffff; opacity: 0.50; font-weight: 100; line-height: 28px; font-feature-settings: 'tnum' 1; text-anchor: end;}"},
+{class:"origin line{fill:none; stroke:#ffffff; stroke-width: 2.0px; opacity: 0.5; stroke-dasharray:1,0}"},
+{class:"link{fill:none; opacity:0.5;}"},
+{class:"circles{opacity: .38}"},
+{class:"circlesHighlight{opacity: 1.0}"}
+],
+titleOffset:36,
+subOffset:0,
+legendyOffset:0,
+linecolours:["#CC4759","#00B5B5","#E6B522","#437099","#FF8C8B","#CBFF7E","#8A8A8A"]
+}
+
+var stylevid = {classes:
+[{class:"background{fill: #212121;}"},
+{class:"titleframe{fill: none;}"},
+{class:"chartholder{fill: none;}"},
+{class:"title{font-size: 68px; fill: #ffffff; font-weight: 600;opacity: 0.9;}"},
+{class:"subtitle{font-size: 48; fill: #ffffff; font-weight: 400;opacity: 0.7;}"},
+{class:"labels{font-size: 48px; fill: #ffffff; opacity: 0.95; font-weight: 400;}"},
+{class:"source{font-size: 36; fill: #ffffff; font-weight: 400;opacity: 0.5;}"},
+{class:"logo{fill: #ffffff;}"},
+{class:"xAxis{fill:none; stroke: none;}"},
+{class:"xAxis line{fill:none; stroke-width: 4px; stroke:#ffffff;opacity: 0.5}"},
+{class:"xAxis text{font-size: 48; fill: #ffffff; font-weight: 400;opacity: 0.5}"},
+{class:"yAxis{fill:none; stroke: none;}"},
+{class:"yAxis line{fill:none; stroke-width: 4px; stroke:#ffffff;opacity: 0.38}"},
+{class:"yAxis text{font-size: 48; fill: #ffffff; font-weight: 400;font-feature-settings: 'tnum' 1; text-anchor: end;opacity: 0.5}"},
+{class:"origin line{fill:none; stroke:#ffffff; stroke-width: 5.0px;stroke-dasharray:1,0; opacity: 0.5}"},
+{class:"whiskers{fill:none; stroke-width:2.0px;stroke:#ffffff; opacity: 0.5}"},
+{class:"lines{fill:none; stroke-width: 8.0px; stroke-linecap: round;}"},
+{class:"fill{fill-opacity:1.0; stroke-width: 0.0px}"},
+{class:"highlight{fill-opacity:1.0;}"},
+{class:"link{fill:none; opacity:0.5;}"},
+{class:"day{fill:none; stroke: #ffffff; stroke-width:4.0px; opacity: 0.5; stroke-linecap: round;}"},
+{class:"month{fill:none; stroke: #ffffff; stroke-width:4.0px; opacity: 0.5; stroke-linecap: round;}"}
+
+],
+titleOffset:68,
+subOffset:0,
+legendyOffset:15,
+linecolours:["#EB3F50","#00D9CA","#BF9413","#1F5E99","#A7FF59","#FF9B96","#81838F"],
+fillcolours:["#EB3F50","#00D9CA","#BF9413","#1F5E99","#A7FF59","#FF9B96","#81838F"]
+}
+
+
+    //Tabular with 
+    //-webkit-font-feature-settings: 'tnum' 1;
+       //font-feature-settings: 'tnum' 1;
